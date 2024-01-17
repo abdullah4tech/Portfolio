f52442964d917760169346ef1a2801e4571e4b9a
Abdullah Mustapha <a41622100@gmail.com>
2024-01-17 19:06:50 +0000

    deleted

--- scripts.js
+++
@@ -1,8 +0,0 @@
-
-
-let MenuBtn = document.getElementById('MenuBtn')
-
-MenuBtn.addEventListener('click', (e) => {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    e.target.classList.toggle('fa-xmark');
-});
