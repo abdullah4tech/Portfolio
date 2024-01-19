1623c2c0d1dd3b0ed54914b2d6ade7b8556d98b7
abdullahCoder-Tech <118620815+abdullahCoder-Tech@users.noreply.github.com>
2024-01-19 01:24:55 +0000

    Updated scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
-
-
-let MenuBtn = document.getElementById('MenuBtn')
+let MenuBtn = document.getElementById('MenuBtn');
 
 MenuBtn.addEventListener('click', (e) => {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    e.target.classList.toggle('fa-xmark');
+  document.querySelector('body').classList.toggle('mobile-nav-active');
+  e.target.classList.toggle('fa-xmark');
 });
-
 
 let typing_effect = new Typed('.auto-input', {
-    strings: ['Font-End Developer!', 'Freelancer!', 'Designer!'],
-    typeSpeed: 100,         
-    backSpeed: 100,
-    backDelay: 2000,
-    loop: true,
+  strings: ['Font-End Developer!', 'Freelancer!', 'Designer!'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 2000,
+  loop: true,
 });
+
+
